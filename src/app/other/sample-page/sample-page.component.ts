009c9cd736017f3506ffdaab4fa36b54510dd1fc
jeserkin <jeserkin@gmail.com>
2020-01-10 18:26:18 +0200

    4 files modified, 3 deleted

--- src/app/other/sample-page/sample-page.component.ts
+++ src/app/other/sample-page/sample-page.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sample-page',
-  templateUrl: './sample-page.component.html',
-  styleUrls: ['./sample-page.component.css']
+  templateUrl: './sample-page.component.html'
 })
 export class SamplePageComponent implements OnInit {
 
